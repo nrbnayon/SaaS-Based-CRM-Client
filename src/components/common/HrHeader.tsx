def1c6852d3c4bdabf33388e84b42c79730b58e6
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-06-24 09:18:53 +0600

    Implement HrHeader component for improved navigation and layout in HrTestPage

---
+++ src/components/common/HrHeader.tsx
@@ -0,0 +1,40 @@
+/** @format */
+
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ModeToggle } from "../ui/mode-toggle";
+import { SquarePlus } from "lucide-react";
+
+const HrHeader = () => {
+  return (
+    <div>
+      <header
+        className={cn(
+          "sticky top-0 z-50 p-4 w-full transition-all duration-200bg-white/10 dark:bg-background/10 backdrop-blur-3xl"
+        )}
+      >
+        <div className=" mx-auto">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-xl md:text-3xl font-bold tracking-tight">
+                Hr Test
+              </h1>
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className="flex items-center cursor-pointer space-x-3">
+              <ModeToggle />
+
+              <div className="flex items-center py-1 px-2 rounded-md bg-black  dark:bg-white text-white dark:text-black cursor-pointer space-x-2">
+                <SquarePlus className="h-4 w-4 " />
+                <p className="  text-xs md:text-base">New Test</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default HrHeader;
