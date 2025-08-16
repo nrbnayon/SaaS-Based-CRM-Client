1a7b4d7d9918134fa5efd2267a32424701a68e56
nrbnayon <nrbnayon@gmail.com>
2025-08-16 23:15:14 +0600

    Added verciel analysis

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,17 @@
 // src/app/(dashboard)/layout.tsx
 import React from "react";
 import DashboardWrapper from "./components/dashboard-wrapper";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DashboardWrapper>{children}</DashboardWrapper>;
+  return (
+    <DashboardWrapper>
+      {children}
+      <Analytics />
+    </DashboardWrapper>
+  );
 }
